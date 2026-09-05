c6360a4219597685d51c6df578d5328d518f3c97
Lucky gupta <guptalucky3692@gmail.com>
2026-09-05 12:50:18 +0530

    Time: 0 ms (100%), Space: 12.4 MB (54.7%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def uniqueOccurrences(self, arr):
+        dic = {}
+        for i in arr:
+            if i in dic:
+                dic[i] +=1
+            else:
+                dic[i] = 1
+        if len(set(dic.values())) != len(set(arr)):
+                return False
+        return True
+            
+
+
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        
